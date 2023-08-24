1c544b9af9cf95272f199eda6a9c1831b7548fc0
Furkan Ongoren <on.fr2021@gmail.com>
2023-08-24 17:04:19 +0100

    update class name

--- src/test/java/practice_/pages/AlloverCommerce_HomePage.java
+++ src/test/java/practice_/pages/AlloverCommerce_HomePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import practice_.utilities.Driver;
 
-public class AC_HomePage {
+public class AlloverCommerce_HomePage {
 
     public AlloverCommerce_HomePage() {
         PageFactory.initElements(Driver.getDriver(), this);

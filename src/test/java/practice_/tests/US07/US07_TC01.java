b44af3f915906c27069fc616223bc9cab562414e
Furkan Ongoren <on.fr2021@gmail.com>
2023-08-25 05:24:06 +0100

    update _Compare pages

--- src/test/java/practice_/tests/US07/US07_TC01.java
+++ src/test/java/practice_/tests/US07/US07_TC01.java
@@ -1,6 +1,7 @@
 package practice_.tests.US07;
 
 import org.testng.annotations.Test;
+import practice_.pages.AlloverCommerce_Compare;
 import practice_.utilities.ConfigReader;
 import practice_.utilities.Driver;
 
@@ -8,6 +9,6 @@ public class US07_TC01 {
 
     @Test
     public void tc01() {
-
+        AlloverCommerce_Compare alloverCommerceCompare = new AlloverCommerce_Compare();
     }
 }

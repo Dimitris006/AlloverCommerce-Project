fc121c280f61315df54b464816732cb78a542789
emillyto <134556319+emillyto@users.noreply.github.com>
2023-08-25 14:39:56 +0100

    F3

--- src/test/java/practice_/tests/US09/US09_TC01.java
+++ src/test/java/practice_/tests/US09/US09_TC01.java
@@ -10,11 +10,14 @@ import practice_.pages.AlloverCommerce_HomePage;
 import practice_.pages.VendorRegistration_Page;
 import practice_.utilities.ConfigReader;
 import practice_.utilities.Driver;
+import practice_.utilities.WaitUtils;
 
 import static org.testng.AssertJUnit.assertTrue;
 
 
 public class US09_TC01 {
+   AlloverCommerce_HomePage alloverCommerceHomePage;
+   VendorRegistration_Page vendorRegistrationPage;
    @Test
     public void vendorSignUp(){
        // Go to home page
@@ -36,6 +39,21 @@ public class US09_TC01 {
 
        // Confirm the Verification code message is visible
        Assert.assertTrue(vendorRegistrationPage.verificationCodeMessage.isDisplayed());
+
+       //Enter verification code in the Verification Code text box
+      vendorRegistrationPage.verificationCode.sendKeys(ConfigReader.getProperty("verification_code"));
+
+      //Enter valid password in Password field
+      vendorRegistrationPage.vendorPassword.sendKeys(ConfigReader.getProperty("vendor_password"));
+
+      //Enter password into Confirm Password field
+      vendorRegistrationPage.vendorConfirmPassword.sendKeys(ConfigReader.getProperty("vendor_password"));
+
+      //Click on Register button
+      vendorRegistrationPage.registerButton.click();
+
+
+
 
 
 

1a81fa06bb9453ab66a996bfb62078d5adf6b92e
emillyto <134556319+emillyto@users.noreply.github.com>
2023-08-25 07:57:09 +0100

    F

--- src/test/java/practice_/tests/US09/US09_TC01.java
+++ src/test/java/practice_/tests/US09/US09_TC01.java
@@ -1,7 +1,13 @@
 
 package practice_.tests.US09;
 
+import org.testng.annotations.Test;
+import practice_.utilities.ConfigReader;
+import practice_.utilities.Driver;
+
 public class US09_TC01 {
-    // thank so much!
-    // He world!
+   @Test
+    public void vendorSignUp(){
+       Driver.getDriver().get(ConfigReader.getProperty("allovercommerce_url"));
+   }
 }

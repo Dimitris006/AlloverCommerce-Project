7c9aa4898023e986c120ee3663275addf658ad21
Nalan Orhan <karadumannalan@gmail.com>
2023-08-26 23:58:48 +0200

    Add US01 test cases

--- src/test/java/practice_/tests/US01/US01_TC05.java
+++ src/test/java/practice_/tests/US01/US01_TC05.java
@@ -3,28 +3,29 @@ package practice_.tests.US01;
 import org.testng.annotations.Test;
 import practice_.pages.AlloverCommerce_HomePage;
 import practice_.pages.AlloverCommerce_MyAccount_Page;
-import practice_.utilities.*;
+import practice_.utilities.BrowserUtils;
+import practice_.utilities.ConfigReader;
+import practice_.utilities.Driver;
+import practice_.utilities.WaitUtils;
 
-import static practice_.utilities.JSUtils.clickWithTimeoutByJS;
+import java.time.Duration;
 
-public class US01_TC01 {
+public class US01_TC05 {
         AlloverCommerce_HomePage alloverCommerceHomePage;
         AlloverCommerce_MyAccount_Page alloverCommerceMyAccountPage;
-
-
         @Test
-        public void TC01() {
+        public void TC05() {
 
                 alloverCommerceHomePage = new AlloverCommerce_HomePage();
                 alloverCommerceMyAccountPage = new AlloverCommerce_MyAccount_Page();
 
                 Driver.getDriver().get(ConfigReader.getProperty("allovercommerce_url"));
                 alloverCommerceHomePage.register_Link.click();
-                alloverCommerceHomePage.signUp_Username.sendKeys("osman123");
-                alloverCommerceHomePage.signUp_Email.sendKeys("osman@gfd.com");
-                alloverCommerceHomePage.signUp_Password.sendKeys("Jackdoe1234.");
+                alloverCommerceHomePage.signUp_Username.sendKeys("4seasons");
+                alloverCommerceHomePage.signUp_Email.sendKeys("4seasons@gfd.com");
+                alloverCommerceHomePage.signUp_Password.sendKeys("JANEDOE1234.");
                 alloverCommerceHomePage.signUp_PrivacyPolicyCheckbox.click();
-                clickWithTimeoutByJS(alloverCommerceHomePage.signUp_Button);
+                alloverCommerceHomePage.signUp_Button.click();
                 WaitUtils.waitFor(2);
                 BrowserUtils.verifyElementDisplayed(alloverCommerceHomePage.signOut_Link);
                 Driver.closeDriver();

79ec7cc379a748d8bc91efbcc86aed14d16ff522
Nalan Orhan <karadumannalan@gmail.com>
2023-08-28 01:41:44 +0200

    US01 test cases with fakers

--- src/test/java/practice_/tests/US01/US01_TC02.java
+++ src/test/java/practice_/tests/US01/US01_TC02.java
@@ -1,5 +1,6 @@
 package practice_.tests.US01;
 
+import com.github.javafaker.Faker;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import practice_.pages.AlloverCommerce_HomePage;
@@ -19,18 +20,23 @@ public class US01_TC02 {
         @Test
         public void TC02() {
 
+                Faker faker = new Faker();
+
                 alloverCommerceHomePage = new AlloverCommerce_HomePage();
                 alloverCommerceMyAccountPage = new AlloverCommerce_MyAccount_Page();
 
+                String username = faker.name().name();
+                System.out.println("username = " + username);
+
                 Driver.getDriver().get(ConfigReader.getProperty("allovercommerce_url"));
                 alloverCommerceHomePage.register_Link.click();
-                alloverCommerceHomePage.signUp_Username.sendKeys("Osman_3");
+                alloverCommerceHomePage.signUp_Username.sendKeys(username);
                 alloverCommerceHomePage.signUp_Email.sendKeys("osman_3@gfd");
                 alloverCommerceHomePage.signUp_Password.sendKeys("Jackdoe1234.");
                 alloverCommerceHomePage.signUp_PrivacyPolicyCheckbox.click();
                 clickWithTimeoutByJS(alloverCommerceHomePage.signUp_Button);
                 WaitUtils.waitFor(2);
-                //BrowserUtils.verifyElementDisplayed(alloverCommerceHomePage.signUp_invalidEmailMessage);
+                BrowserUtils.verifyElementDisplayed(alloverCommerceHomePage.signUp_invalidEmailMessage);
                 Assert.assertTrue(alloverCommerceHomePage.signUp_invalidEmailMessage.isDisplayed());
                 Driver.closeDriver();
 

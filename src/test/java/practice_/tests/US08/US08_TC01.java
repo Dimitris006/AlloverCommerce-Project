893273d99cf00cee8ef684e8372e48512501b820
Furkan Ongoren <on.fr21@gmail.com>
2023-08-28 01:33:48 +0100

    comment out dublicate class in same file! Check with team

--- src/test/java/practice_/tests/US08/US08_TC01.java
+++ src/test/java/practice_/tests/US08/US08_TC01.java
@@ -75,31 +75,31 @@ public class US08_TC01 {
 
 
 
-    public class US08_TC01 {
-        @Test
-        public void testcase01() {
-            AlloverCommerce_HomePage alloverCommerceHomePage = new AlloverCommerce_HomePage();
-            AlloverCommerce_Wishlist_Page alloverCommerceWishlistPage = new AlloverCommerce_Wishlist_Page();
-            //Go to homepage
-            Driver.getDriver().get(ConfigReader.getProperty("allovercommerce_url"));
-
-            //Click on Search box write product name in Search box
-            //Click the search button
-            WaitUtils.waitFor(2);
-
-            alloverCommerceHomePage.searchBox.click();
-            alloverCommerceHomePage.searchBox.sendKeys("Automatic Crusher", Keys.ENTER);
-
-            //Click on wishlist icon
-            alloverCommerceWishlistPage.homepage_Add_Wishlist_Button.click();
-            WaitUtils.waitFor(3);
-
-            //Click the Wishlist button
-            alloverCommerceWishlistPage.wishList_Button.click();
-
-            //Verify the product is added into the wishlist
-            WaitUtils.waitFor(3);
-            alloverCommerceWishlistPage.verifyAddedProductToWishList.isDisplayed();
-        }
-    }
+//    public class US08_TC01 {
+//        @Test
+//        public void testcase01() {
+//            AlloverCommerce_HomePage alloverCommerceHomePage = new AlloverCommerce_HomePage();
+//            AlloverCommerce_Wishlist_Page alloverCommerceWishlistPage = new AlloverCommerce_Wishlist_Page();
+//            //Go to homepage
+//            Driver.getDriver().get(ConfigReader.getProperty("allovercommerce_url"));
+//
+//            //Click on Search box write product name in Search box
+//            //Click the search button
+//            WaitUtils.waitFor(2);
+//
+//            alloverCommerceHomePage.searchBox.click();
+//            alloverCommerceHomePage.searchBox.sendKeys("Automatic Crusher", Keys.ENTER);
+//
+//            //Click on wishlist icon
+//            alloverCommerceWishlistPage.homepage_Add_Wishlist_Button.click();
+//            WaitUtils.waitFor(3);
+//
+//            //Click the Wishlist button
+//            alloverCommerceWishlistPage.wishList_Button.click();
+//
+//            //Verify the product is added into the wishlist
+//            WaitUtils.waitFor(3);
+//            alloverCommerceWishlistPage.verifyAddedProductToWishList.isDisplayed();
+//        }
+//    }
 }

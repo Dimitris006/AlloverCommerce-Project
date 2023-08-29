beee9496eb1a78c22563bd971591c36eadaf4729
emillyto <134556319+emillyto@users.noreply.github.com>
2023-08-29 23:16:42 +0100

    Final HP2

--- src/test/java/practice_/pages/AlloverCommerce_AddressesPage.java
+++ src/test/java/practice_/pages/AlloverCommerce_AddressesPage.java
@@ -11,7 +11,7 @@ public class AlloverCommerce_AddressesPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    //Addresses Page Locators
+        //Addresses Page Locators
 
     //Shipping Address Locators
     @FindBy (xpath = "//h4[.='Addresses']")
@@ -23,7 +23,6 @@ public class AlloverCommerce_AddressesPage {
     @FindBy (xpath = "//*[@href='https://allovercommerce.com/my-account-2/edit-address/shipping/']")
     public WebElement shippingAddressAdd;
 
-
     @FindBy (xpath = "//*[@name='shipping_last_name']")
     public WebElement lastNameField;
 
@@ -32,6 +31,9 @@ public class AlloverCommerce_AddressesPage {
 
     @FindBy (xpath = "//*[@id='select2-shipping_country-container']")
     public WebElement countryDropdwn;
+
+    @FindBy (id = "billing_country")
+    public WebElement countryDropdown;
 
     @FindBy (xpath = "//*[@name='shipping_address_1']")
     public WebElement streetAddress;
@@ -43,6 +45,7 @@ public class AlloverCommerce_AddressesPage {
     public WebElement stateDropdown;
 
     @FindBy (xpath = "//*[@name='shipping_postcode']")
+
     public WebElement zipCode;
 
     @FindBy (xpath = "//*[@name='save_address']")
@@ -64,8 +67,8 @@ public class AlloverCommerce_AddressesPage {
     @FindBy (xpath = "//*[@name='billing_company']")
     public WebElement billCompanyName;
 
-    @FindBy (xpath = "//*[@id='select2-billing_country-container']")
-    public WebElement billCountryDropdown;
+        //    @FindBy (xpath = "//*[@id='select2-billing_country-container']")
+//    public WebElement billCountryDropdown;
 
     @FindBy (xpath = "//*[@name='billing_address_1']")
     public WebElement billStreetAddress;
@@ -82,12 +85,82 @@ public class AlloverCommerce_AddressesPage {
     @FindBy (xpath = "//*[@name='billing_phone']")
     public WebElement billPhone;
 
-
-//    @FindBy (xpath = "//*[@name='save_address']")
+        //    @FindBy (xpath = "//*[@name='save_address']")
 //    public WebElement saveAddressButton;
 //
 //    @FindBy (xpath = "//*[@class = 'fas fa-check']")
 //    public WebElement succesfullAdd;
 
+    @FindBy (xpath = "//*[@data-id='shipping_first_name']")
+    public WebElement firstNameWrong;
+
+    @FindBy (xpath = "//*[@data-id='shipping_last_name']")
+    public WebElement lastNameWrong;
+
+    @FindBy (xpath = "//*[@data-id='shipping_country']")
+    public WebElement countryWrong;
+
+    @FindBy (xpath = "//*[@data-id='shipping_address_1']")
+    public WebElement addressWrong;
+
+    @FindBy (xpath = "//*[@data-id='shipping_city']")
+    public WebElement cityWrong;
+
+    @FindBy (xpath = "//*[@data-id='shipping_state']")
+    public WebElement stateWrong;
+
+    @FindBy (xpath = "//*[@data-id='shipping_postcode']")
+    public WebElement postcodeWrong;
+
+    @FindBy (xpath = "//*[@name='save_address']")
+    public WebElement billSaveAddressButton;
+
+    @FindBy (xpath = "//*[@id='billing_first_name']")
+    public WebElement billFirstNameValue;
+
+    @FindBy (xpath = "//*[@name='billing_last_name']")
+    public WebElement billLastNameValue;
+
+    @FindBy (xpath = "//*[@data-id='billing_country']")
+    public WebElement billCountryWrong;
+
+    @FindBy (xpath = "//*[@data-id='billing_address_1']")
+    public WebElement billAddressWrong;
+
+    @FindBy (xpath = "//*[@data-id='billing_city']")
+    public WebElement billCityWrong;
+
+    @FindBy (xpath = "//*[@data-id='billing_state']")
+    public WebElement billStateWrong;
+
+    @FindBy (xpath = "//*[@data-id='billing_postcode']")
+    public WebElement billPostcodeWrong;
+
+    @FindBy (xpath = "//*[@data-id='billing_phone']")
+    public WebElement billPhoneWrong;
+
+    @FindBy (xpath = "//select[@id='shipping_country']")
+    public WebElement shipCountryDropdown;
+
+    @FindBy (xpath = "//select[@id='billing_country']")
+    public WebElement billCountryDropdown;
+
+    @FindBy(id="shipping_postcode")
+    public WebElement ukPostCode;
+
+
+        //    @FindBy (xpath = "//*[@class = 'fas fa-check']")
+//    public WebElement succesfullAdd;
 
 }
+
+
+
+
+
+
+
+
+
+
+

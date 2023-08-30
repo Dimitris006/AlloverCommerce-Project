c1a2993c080fa8f8052f162a1983c6f04b576305
Nalan Orhan <karadumannalan@gmail.com>
2023-08-30 07:48:29 +0200

    us06 last v

--- src/test/java/practice_/tests/US06/US06_TC07.java
+++ src/test/java/practice_/tests/US06/US06_TC07.java
@@ -21,6 +21,7 @@ public class US06_TC07 {
         //navigate to URL
         Driver.getDriver().get(ConfigReader.getProperty("allovercommerce_url"));
 
+        //Customer sign-in
         alloverCommerceHomePage.signIn_Link.click();
         alloverCommerceHomePage.signIn_Username.sendKeys(ConfigReader.getProperty("customer_email"));
         alloverCommerceHomePage.signIn_Password.sendKeys(ConfigReader.getProperty("customer_password"));
